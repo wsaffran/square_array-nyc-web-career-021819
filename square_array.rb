86bab75737ab75d16ac10cb9bdc709fb904159c3
Will Saffran <willh.saffran@gmail.com>
2019-02-05 19:02:38 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,7 @@
 def square_array(array)
   new_array = []
-  i = 0
   array.each do |item|
-    new_array[i] = item ** item
-    i++
+    new_array.push(item ** item)
   end
   return new_array
 end
